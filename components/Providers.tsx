94b9bc2f115b4bea108a4163df0c69222406306a
Purnet Moirangthem <purnet.moirangthem@kapturecrm.com>
2026-04-17 23:10:45 +0530

    feat: add ThemeProvider client wrapper

---
+++ components/Providers.tsx
@@ -0,0 +1,17 @@
+// components/Providers.tsx
+"use client";
+
+import { ThemeProvider } from "next-themes";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange={false}
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
